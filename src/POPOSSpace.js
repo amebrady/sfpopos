2786da7b2b19b827a3e4e7800806e80bd554c569
Amelia <amelia.brady@students.dominican.edu>
2022-12-10 21:16:01 -0800

    dynamic content and footer

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -1,10 +1,11 @@
-
+import './POPOSSpace.css'
 
 function POPOSSpace(props) {
    const { name, image, address } = props
   return (
     <div>
-    <img src={`${process.env.PUBLIC_URL}/images/50-california-st.jpg`}
+    <img
+      src={`${process.env.PUBLIC_URL}/images/${image}`}
       width="300"
       height="300"
       alt="50 Califonia St."

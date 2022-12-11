b1a43264a6f0fb1893a3096b9b38698f377ebcf5
Amelia <amelia.brady@students.dominican.edu>
2022-12-10 21:10:17 -0800

    content component built

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './App.css';
 import Title from './Title.js'
+import POPOSList from './POPOSList.js';
+
 function App() {
   return (
     <div className="App">
       <Title />
+      <POPOSList />
     </div>
   );
 }
